bee8ff71c2dfb91b22d6c2cf547f0f63e9eba6ed
Jake Nelson <j.n@live.com.au>
2019-11-06 06:30:46 +0800

    Minimal working example

--- test-tabbar-swfitui/ContentView.swift
+++ test-tabbar-swfitui/ContentView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
     @State private var selection = 0
@@ -31,6 +32,97 @@ struct ContentView: View {
                     }
                 }
                 .tag(1)
+        }
+    }
+}
+
+struct UIKitTabView: View {
+    private var viewControllers: [UIHostingController<AnyView>]
+    @State var selectedIndex: Int = 0
+    
+    init(_ views: [Tab]) {
+        self.viewControllers = views.map {
+            let host = UIHostingController(rootView: $0.view)
+            host.tabBarItem = $0.barItem
+            return host
+        }
+    }
+    
+    var body: some View {
+        TabBarController(controllers: viewControllers, selectedIndex: $selectedIndex)
+            .edgesIgnoringSafeArea(.all)
+    }
+    
+    struct Tab {
+        var view: AnyView
+        var barItem: UITabBarItem
+        
+        init<V: View>(view: V, barItem: UITabBarItem) {
+            self.view = AnyView(view)
+            self.barItem = barItem
+        }
+        
+        // convenience
+        init<V: View>(view: V, title: String?, image: String, selectedImage: String? = nil) {
+            let selectedImage = selectedImage != nil ? UIImage(named: selectedImage!) : nil
+            let barItem = UITabBarItem(title: title, image: UIImage(named: image), selectedImage: selectedImage)
+            self.init(view: view, barItem: barItem)
+        }
+    }
+}
+
+struct TabBarController: UIViewControllerRepresentable {
+    var controllers: [UIViewController]
+    @Binding var selectedIndex: Int
+
+    func makeUIViewController(context: Context) -> UITabBarController {
+        let tabBarController = UITabBarController()
+        tabBarController.viewControllers = controllers
+        tabBarController.delegate = context.coordinator
+        tabBarController.selectedIndex = 0
+        return tabBarController
+    }
+
+    func updateUIViewController(_ tabBarController: UITabBarController, context: Context) {
+        tabBarController.selectedIndex = selectedIndex
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UITabBarControllerDelegate {
+        var parent: TabBarController
+
+        init(_ tabBarController: TabBarController) {
+            self.parent = tabBarController
+        }
+        
+        func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+            parent.selectedIndex = tabBarController.selectedIndex
+        }
+    }
+}
+
+struct ExampleView: View {
+    @State var text: String = ""
+    
+    var body: some View {
+        UIKitTabView([
+            UIKitTabView.Tab(view: NavView(), title: "First", image: ""),
+            UIKitTabView.Tab(view: Text("Second View"), title: "Second", image: "")
+        ])
+    }
+}
+
+struct NavView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                NavigationLink(destination: Text("This page stays when you switch back and forth between tabs (as expected on iOS)")) {
+                    Text("Go to detail")
+                }
+            }
         }
     }
 }
